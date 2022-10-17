f7c49aad6b37a8162044390057786cf4e8be021b
Richard Backhouse <richard@Richards-MacBook-Pro-2.local>
2022-10-17 21:24:21 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,10 @@
+name = 'Richard'
+day = 1
+week = 1
+
+def day_review(name, day, week):
+    print("My name is " + name + " and I really enjoyed day",day,"of week" ,week, "at codeclan!")
+
+day_review(name, day, week)
+
+print("I can't wait for day",(day+1),"!")
